8b651eca0b98ad1273971e194814df6308715fda
Dahrahcme <tripleo4real@gmail.com>
2023-01-03 08:41:34 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * main - a function that fills memory with a constant byte
  *
- * Returns: to a pointer to the memory area s
+ * Return: to a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n);
 {
