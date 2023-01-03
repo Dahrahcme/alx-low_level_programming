d599191b72439e8e29217238093a0f1f4853fb40
Dahrahcme <tripleo4real@gmail.com>
2023-01-03 09:38:22 -0800

    Task 1: 0.2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
 		_putchar(alpha);
-	}
 
 	_putchar('\n');
 }
