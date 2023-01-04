0c4c822401d14d4ea0acf41d0b1950eff9b1e80e
Dahrahcme <tripleo4real@gmail.com>
2023-01-04 10:13:15 -0800

    task 2: 0.1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,8 +3,9 @@
 /**
  * _strlen_recursion - returns the length of a string
  *
- * @s: the string
+ * @s: the string to be measured
  *
+ * Return: the length of the string
  */
 
 int _strlen_recursion(char *s)
